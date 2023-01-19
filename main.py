3f07afb2eadba6fb08b487a8105a08f0d0d3434c
jatin161 <35172881+jatin161@users.noreply.github.com>
2023-01-19 19:38:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,9 +64,9 @@ def main():
                 st.write(sentiment)
 
                 # Emoji
-                if sentiment.polarity > 0.5:
+                if sentiment.polarity > 0.1:
                     st.markdown("Sentiment:: Positive :smiley: ")
-                elif sentiment.polarity < -0.5:
+                elif sentiment.polarity < -0.1:
                     st.markdown("Sentiment:: Negative :angry: ")
                 else:
                     st.markdown("Sentiment:: Neutral 😐 ")
